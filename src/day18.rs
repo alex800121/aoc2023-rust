4c40c197288af48030b73c2fd24750c708856cfe
alex800121 <alex800121@hotmail.com>
2024-03-10 07:55:26 +0800

    day19

--- src/day18.rs
+++ src/day18.rs
@@ -1,10 +1,89 @@
+use aoc2023::Direction::{self, *};
 use project_root::get_project_root;
+use std::i64;
 
+fn read_input(input: &str) -> Option<(Direction, f64)> {
+    let mut input = input.split_whitespace();
+    Some((
+        match input.next()? {
+            "U" => North,
+            "L" => West,
+            "R" => East,
+            "D" => South,
+            _ => panic!("wrong char"),
+        },
+        input.next()?.parse::<f64>().ok()?,
+    ))
+}
+type V = Vec<(Direction, f64)>;
+type Index = (f64, f64);
+fn calc_corners(d0: &Direction, d1: &Direction, (x, y): &Index) -> (Index, Index) {
+    match d0 {
+        South => match d1 {
+            East => ((x + 0.5, y - 0.5), (x - 0.5, y + 0.5)),
+            West => ((x + 0.5, y + 0.5), (x - 0.5, y - 0.5)),
+            _ => panic!("wrong d"),
+        },
+        North => match d1 {
+            East => ((x - 0.5, y - 0.5), (x + 0.5, y + 0.5)),
+            West => ((x - 0.5, y + 0.5), (x + 0.5, y - 0.5)),
+            _ => panic!("wrong d"),
+        },
+        _ => calc_corners(d1, d0, &(*x, *y)), 
+    }
+}
+fn calc_edges(input: &V) -> (Vec<Index>, Vec<Index>) {
+    let mut start = (0.0, 0.0);
+    let mut input = input.iter().peekable();
+    let mut v0 = Vec::new();
+    let mut v1 = Vec::new();
+    while let (Some((d0, _)), Some((d1, n1))) = (input.next(), input.peek()) {
+        let (p, s) = calc_corners(d0, d1, &start);
+        v0.push(p);
+        v1.push(s);
+        start = (
+            start.0 + (d1.to_index().0 as f64 * n1),
+            start.1 + (d1.to_index().1 as f64 * n1),
+        );
+    }
+    (v0, v1)
+}
+fn calc_area(v: &Vec<Index>) -> f64 {
+    let v0 = v.first().unwrap();
+    let mut v = v.clone();
+    v.push(*v0);
+    let mut v = v.iter().peekable();
+    let mut acc = 0.0f64;
+    while let (Some((x0, y0)), Some((x1, y1))) = (v.next(), v.peek()) {
+        acc += x0 * y1 - y0 * x1;
+    }
+    acc.abs() / 2.0
+}
+fn read_input_b(input: &str) -> Option<(Direction, f64)> {
+    let (a, b) = input.split_whitespace().nth(2)?.trim_start_matches("(#").trim_end_matches(')').split_at(5);
+    // dbg!(&a, &b);
+    let b = (match b {
+        "0" => Some(East),
+        "1" => Some(South),
+        "2" => Some(West),
+        "3" => Some(North),
+        _ => None,
+    })?;
+    Some((b, i64::from_str_radix(a, 16).ok()? as f64))
+}
 pub fn run(day: usize) {
-    // let input = std::fs::read_to_string(format!(
-    //     "{}/input/input{:02}.txt",
-    //     get_project_root().unwrap().to_str().unwrap(),
-    //     day
-    // ))
-    // .unwrap();
+    let input = std::fs::read_to_string(format!(
+        "{}/input/input{:02}.txt",
+        get_project_root().unwrap().to_str().unwrap(),
+        day
+    ))
+    .unwrap();
+    let mut ins_a = input.lines().filter_map(read_input).collect::<Vec<_>>();
+    ins_a.push(*ins_a.first().unwrap());
+    let (a, b) = calc_edges(&ins_a);
+    println!("day18a: {}", calc_area(&a).max(calc_area(&b)));
+    let mut ins_a = input.lines().filter_map(read_input_b).collect::<Vec<_>>();
+    ins_a.push(*ins_a.first().unwrap());
+    let (a, b) = calc_edges(&ins_a);
+    println!("day18b: {}", calc_area(&a).max(calc_area(&b)));
 }
--- src/day19.rs
+++ src/day19.rs
@@ -1,10 +1,166 @@
+use std::collections::HashMap;
+
+use nom::{
+    branch::alt,
+    character::complete::{self, alpha1, char, newline},
+    combinator::{map, value},
+    multi::separated_list1,
+    sequence::{delimited, terminated},
+    sequence::{preceded, tuple},
+    IResult,
+};
 use project_root::get_project_root;
 
+type Ins<'a> = HashMap<&'a str, Vec<(Cond, Dest<'a>)>>;
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Hash)]
+enum Cond {
+    LessThan(usize, u32),
+    GreaterThan(usize, u32),
+    Pass,
+}
+#[derive(Clone, PartialEq, Copy, Eq, PartialOrd, Ord, Debug, Hash)]
+enum Dest<'a> {
+    Accepted,
+    Rejected,
+    Field(&'a str),
+}
+fn xmas_parser(input: &str) -> IResult<&str, usize> {
+    alt((
+        value(0, char('x')),
+        value(1, char('m')),
+        value(2, char('a')),
+        value(3, char('s')),
+    ))(input)
+}
+fn dest_parser(input: &str) -> IResult<&str, Dest<'_>> {
+    use Dest::*;
+    alt((
+        value(Accepted, char('A')),
+        value(Rejected, char('R')),
+        map(alpha1, Field),
+    ))(input)
+}
+fn cond_parser_0(input: &str) -> IResult<&str, Cond> {
+    use Cond::*;
+    let (input, f) = xmas_parser(input)?;
+    let (input, c) = alt::<_, _, _, _>((char('<'), char('>')))(input)?;
+    let (input, i) = terminated(complete::u32, char(':'))(input)?;
+    Ok((
+        input,
+        if c == '<' {
+            LessThan(f, i)
+        } else {
+            GreaterThan(f, i)
+        },
+    ))
+}
+fn cond_parser(input: &str) -> IResult<&str, Cond> {
+    cond_parser_0(input).or(Ok((input, Cond::Pass)))
+}
+type Xmas = [u32; 4];
+type Range = (u32, u32);
+fn ins_parser(input: &str) -> IResult<&str, (&str, Vec<(Cond, Dest<'_>)>)> {
+    let (input, name) = alpha1(input)?;
+    let (input, x) = delimited(
+        char('{'),
+        separated_list1(char(','), tuple((cond_parser, dest_parser))),
+        char('}'),
+    )(input)?;
+    Ok((input, (name, x)))
+}
+fn input_parser(input: &str) -> IResult<&str, (Ins<'_>, Vec<Xmas>)> {
+    let (input, m) = separated_list1(newline, ins_parser)(input)?;
+    let (input, _) = tuple((newline, newline))(input)?;
+    let (input, l) = separated_list1(
+        newline,
+        delimited(
+            char('{'),
+            separated_list1(
+                char(','),
+                preceded(tuple((xmas_parser, char('='))), complete::u32),
+            ),
+            char('}'),
+        ),
+    )(input)?;
+    Ok((input, (HashMap::from_iter(m), l.into_iter().map(|x| x.try_into().unwrap()).collect())))
+}
+type XmasPlus = [Range; 4];
+fn fill_next<'a>(cond: &Cond, dest: &Dest<'a>, r: XmasPlus) -> Vec<(XmasPlus, Dest<'a>)> {
+    use Cond::*;
+    let mut v = Vec::new();
+    let mut init = vec![r];
+    let mut rest = Vec::new();
+    while !init.is_empty() {
+        dbg!(init.len());
+        for r in init {
+            match cond { Pass => v.push((r, *dest)),
+                LessThan(f, i) => {
+                    match r[*f] {
+                        (_, n) if n <= *i => v.push((r, *dest)),
+                        (m, n) if m < *i => {
+                            let mut x = r;
+                            x[*f] = (m, *i);
+                            v.push((x, *dest));
+                            x[*f] = (*i, n);
+                            rest.push(x);
+                        }
+                        _ => rest.push(r),
+                    }
+                },
+                GreaterThan(f, i) => {
+                    match r[*f] {
+                        (m, _) if m > *i => v.push((r, *dest)),
+                        (m, n) if n > *i + 1 => {
+                            let mut x = r;
+                            x[*f] = (*i + 1, n);
+                            v.push((x, *dest));
+                            x[*f] = (m, *i + 1);
+                            rest.push(x);
+                        }
+                        _ => rest.push(r),
+                    }
+                }
+            }
+        }
+        init = std::mem::take(&mut rest);
+    }
+    v
+}
+fn calc_range(r: Range, start: &str, ins: Ins<'_>) -> (Vec<XmasPlus>, Vec<XmasPlus>) {
+    use Dest::*;
+    let mut accepted = Vec::new();
+    let mut rejected = Vec::new();
+    let mut init_range = vec![([r; 4], Field(start))];
+    let mut next_range = Vec::new();
+    while !init_range.is_empty() {
+        for (r, s) in init_range {
+            match s {
+                Accepted => accepted.push(r),
+                Rejected => rejected.push(r),
+                Field(s) => {
+                    if let Some(cond) = ins.get(s) {
+                        for (c, i) in cond {
+                            let v = fill_next(c, i, r);
+                            next_range.extend(v);
+                        }
+                    }
+                }
+            }
+        }
+        init_range = std::mem::take(&mut next_range);
+    }
+    (accepted, rejected)
+}
+
 pub fn run(day: usize) {
-    // let input = std::fs::read_to_string(format!(
-    //     "{}/input/input{:02}.txt",
-    //     get_project_root().unwrap().to_str().unwrap(),
-    //     day
-    // ))
-    // .unwrap();
+    let input = std::fs::read_to_string(format!(
+        "{}/input/test{:02}.txt",
+        // "{}/input/input{:02}.txt",
+        get_project_root().unwrap().to_str().unwrap(),
+        day
+    ))
+    .unwrap();
+    let (ins, v) = input_parser(&input).unwrap().1;
+    dbg!(calc_range((1, 4001), "in", ins).0.iter().map(|x| x.iter().map(|(a, b)| b - a).product::<u32>()).sum::<u32>());
 }
